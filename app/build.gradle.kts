d819938eea09cb580c5c5f5a27e612a466b3b50c
saini128 <saini.hck@gmail.com>
2025-09-05 02:57:15 +0530

    widget with correct color constrast

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.singhropar.messmenu"
         minSdk = 31
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
